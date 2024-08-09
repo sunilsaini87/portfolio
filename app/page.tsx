1482b629fe67b9737d6bb8ebb7ad18f1a14c7b1f
sunilsaini87 <sunilkumarsaini6363@gmail.com>
2024-08-09 14:12:17 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
   return (
