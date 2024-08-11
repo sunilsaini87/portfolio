2a1a0c746ba6150994904b75dbfcc1b8b32554b4
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2024-08-11 23:40:18 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import About from "@/components/About";
-// import { Features } from "@/components/Features";
 import MyProjects from "@/components/MyProjects";
 import MySkills from "@/components/MySkills";
 
@@ -10,7 +9,6 @@ export default function Home() {
         <About />
         <MySkills />
         <MyProjects />
-        {/* <Features /> */}
       </div>
     </main>
   );
